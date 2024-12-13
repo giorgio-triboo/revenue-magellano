87e3815f32c85c78c4e274fce20dc7173d5bbce7
triboo-direct <giorgio.contarini@triboo.it>
2024-12-13 17:06:51 +0100

    aggiunto adminer

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -12,15 +12,21 @@ class SettingsController extends Controller
             'adminerUrl' => 'http://localhost:8082'
         ]);
     }
+
     public function adminer()
-    {
-        $path = base_path('adminer/adminer.php');
+{
+    \Log::debug('Entrato nel metodo adminer.');
 
-        if (!file_exists($path)) {
-            abort(404, 'Adminer non trovato.');
-        }
+    $path = base_path('adminer/adminer.php');
+    \Log::debug("Percorso Adminer: $path");
 
-        return response()->file($path);
+    if (!file_exists($path)) {
+        \Log::debug('Adminer non trovato.');
+        abort(404, 'Adminer non trovato.');
     }
+
+    return response()->file($path);
+}
+
 
 }
