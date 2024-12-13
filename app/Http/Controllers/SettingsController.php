353f2e155503ca3e5dac8e424b2ea45a4d12fd2a
triboo-direct <giorgio.contarini@triboo.it>
2024-12-13 17:17:14 +0100

    aggiunto adminer

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -7,25 +7,24 @@ class SettingsController extends Controller
     public function index()
     {
         $this->authorize('view', 'App\Models\Settings');
-        
+
         return view('settings.index', [
             'adminerUrl' => 'http://localhost:8082'
         ]);
     }
 
     public function adminer()
-{
-    $path = resource_path('views/settings/adminer/adminer.php');
+    {
+        $adminerPath = resource_path('views/settings/adminer/adminer.php');
 
-    \Log::debug("Percorso Adminer: $path");
+        if (!file_exists($adminerPath)) {
+            abort(404, 'Adminer non trovato.');
+        }
 
-    if (!file_exists($path)) {
-        \Log::debug('Adminer non trovato.');
-        abort(404, 'Adminer non trovato.');
+        // Includi il contenuto di Adminer come parte della risposta
+        require_once $adminerPath;
+        exit; // Termina l'esecuzione del framework dopo aver caricato Adminer
     }
-
-    return response()->file($path);
-}
 
 
 
