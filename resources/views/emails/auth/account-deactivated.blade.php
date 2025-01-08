96a55f2c55761f2c1248a8360bf690294c6ee3f1
triboo-direct <giorgio.contarini@triboo.it>
2025-01-08 15:13:41 +0100

    modificata mail deactivate account

--- resources/views/emails/auth/account-deactivated.blade.php
+++ resources/views/emails/auth/account-deactivated.blade.php
@@ -5,7 +5,7 @@ Ciao {{ $user->first_name }},
 
 Il tuo account è stato **disattivato**. 
 
-Se ritieni che questo sia un errore o desideri riattivare il tuo account, invia una mail a pannello@triboo.direct.
+Se ritieni che questo sia un errore o desideri riattivare il tuo account, invia una mail a revenue@magellano.ai.
 
 Grazie,<br>
 {{ config('app.name') }}
