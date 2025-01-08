dda2765298c4072d4033fd4e0af31a3b3ad013a7
triboo-direct <giorgio.contarini@triboo.it>
2025-01-08 18:23:02 +0100

    ripristinato statements originario

--- resources/views/statements/index.blade.php
+++ resources/views/statements/index.blade.php
@@ -19,6 +19,8 @@
             </div>
         </div>
 
+
+
         <div class="mt-8 space-y-8">
             <!-- Filters & Quick Stats -->
             <div class="grid gap-8 md:grid-cols-3">
@@ -140,14 +142,7 @@
                     </div>
 
                     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                        @php
-                            $currentMonth = now()->month;
-                            $displayMonths = collect($monthlyStats)->filter(function ($stat) use ($currentMonth) {
-                                return $stat['month_number'] < $currentMonth;
-                            });
-                        @endphp
-
-                        @forelse($displayMonths as $stat)
+                        @forelse($monthlyStats as $stat)
                             <a href="{{ route('statements.details', array_merge(request()->query(), ['statement_month' => $stat['month_number']])) }}"
                                 class="block">
                                 <div
@@ -170,4 +165,5 @@
             </div>
         </div>
     </div>
+
 @endsection
