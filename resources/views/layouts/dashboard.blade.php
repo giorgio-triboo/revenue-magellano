b611df9c270b0b2847f5c38c337565709fa85d6f
triboo-direct <giorgio.contarini@triboo.it>
2025-01-13 15:37:19 +0100

    aggiunta favicon

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/magellano-ico.png') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="/{{ asset('img/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="/{{ asset('img/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="/{{ asset('img/favicon-16x16.png') }}">
-    <link rel="manifest" href="/{{ asset('img/site.webmanifest') }}">
-    <link rel="mask-icon" href="/{{ asset('img/safari-pinned-tab.svg') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('img/site.webmanifest') }}">
+    <link rel="mask-icon" href="{{ asset('img/safari-pinned-tab.svg') }}">
     <title>{{ config('app.name') }} - @yield('title')</title>
 
     <!-- Poppins Font -->
