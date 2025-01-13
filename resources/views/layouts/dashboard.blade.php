e0485b2ab16cccc246abc735c44603676a4e55f1
triboo-direct <giorgio.contarini@triboo.it>
2025-01-13 15:28:32 +0100

    aggiunta favicon

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/magellano-ico.png') }}">
     <title>{{ config('app.name') }} - @yield('title')</title>
 
     <!-- Poppins Font -->
