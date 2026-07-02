e9ad6ddfa78fea5bb08f3dbb07f49d6b9281f934
Giorgio Contarini <giorgio.contarini@contagio-bot.com>
2026-07-02 11:07:47 +0200

    Fix user details modal rendering

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -211,15 +211,14 @@
     </div>
 
     <!-- User Details Modal -->
-    <div x-show="showModal" class="fixed z-10 inset-0 overflow-y-auto" x-cloak>
-        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+    <div x-show="showModal" class="fixed inset-0 z-50 overflow-y-auto" x-cloak aria-labelledby="user-details-modal"
+        role="dialog" aria-modal="true">
+        <div class="flex min-h-screen items-center justify-center p-4 text-center sm:p-0">
             <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"
                 @click="showModal = false"></div>
 
-            <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-
             <div
-                class="inline-block align-bottom bg-white rounded-xl px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-2xl sm:w-full sm:p-6">
+                class="relative transform overflow-hidden rounded-xl bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6">
                 <div class="absolute top-0 right-0 pt-4 pr-4">
                     <button type="button" @click="showModal = false"
                         class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-custom-activeItem">
@@ -231,7 +230,7 @@
                 <div class="sm:flex sm:items-start">
                     <div class="mt-3 text-center sm:mt-0 sm:text-left w-full">
                         <div class="flex items-center justify-between">
-                            <h3 class="text-lg leading-6 font-medium text-gray-900"
+                            <h3 id="user-details-modal" class="text-lg leading-6 font-medium text-gray-900"
                                 x-text="userDetails ? userDetails.first_name + ' ' + userDetails.last_name : ''"></h3>
                         </div>
 
@@ -338,7 +337,7 @@
 
                 <!-- Footer Actions -->
                 <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
-                    <a :href="userDetails ? `/users/${userDetails . id}/edit` : '#'"
+                    <a :href="userDetails ? `/users/${userDetails.id}/edit` : '#'"
                         class="inline-flex justify-center py-2 px-4 border border-transparent rounded-xl shadow-sm text-md font-medium text-white bg-custom-activeItem hover:bg-custom-activeItem/90 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-custom-activeItem">
                         Modifica
                     </a>
